b0567b3282a5d7bdf56f3f1447de7bd210c0d83b
ericsheridanwhs <eric.sheridan@whitehatsec.com>
2020-12-08 14:45:27 -0500

    search

--- app/sqli_1.php
+++ app/sqli_1.php
@@ -141,6 +141,7 @@ if(isset($_GET["title"]))
     $title = $_GET["title"];
     
     // $title = str_replace($title, "'", "");
+    
     $sql = "SELECT * FROM movies WHERE title LIKE '%" . sqli($title) . "%'";
 
     $recordset = mysql_query($sql, $link);
