8fcad74cf8c2d0ff6a5cff9ec7188bb68e15a881
scanner <scanner@whitehatsec.com>
2022-01-24 11:24:25 -0500

    Rays Update

--- app/ba_forgotten.php
+++ app/ba_forgotten.php
@@ -13,7 +13,7 @@ Malik Mesellem
 Twitter: @MME_IT
 
 bWAPP is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License (http://creativecommons.org/licenses/by-nc-nd/4.0/). Copyright © 2014 MME BVBA. All rights reserved.
-
+Ray Test
 */
 
 include("security.php");
